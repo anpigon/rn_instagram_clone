f0a268ccd69208a18aa0245e388d1d542972f6ba
markan <markan@nexg.net>
2019-01-22 10:52:47 +0900

    #3

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
-import { Container, Content, Icon } from 'native-base';
+import { Container, Content, Icon, Thumbnail } from 'native-base';
 import CardComponent from '../CardComponent';
  
 export default class HomeTab extends Component {
@@ -13,7 +13,8 @@ export default class HomeTab extends Component {
     }
 
     state = {
-        feeds: []
+        feeds: [],
+        followings: []
     }
 
     componentWillMount() {
@@ -22,6 +23,31 @@ export default class HomeTab extends Component {
                 feeds
             })
         });
+        this.fetchFollowing().then(followings => {
+            this.setState({
+                followings
+            })
+        });
+    }
+
+    fetchFollowing() {
+        const data = {
+            id: 2,
+            jsonrpc: "2.0",
+            method: "call",
+            params: [
+              "follow_api",
+              "get_following",
+              ["anpigon", "", "blog", 10]
+            ]
+        };
+        return fetch('https://api.steemit.com',
+        {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(res => res.result.map(({following}) => following))
     }
 
     fetchFeeds() {
@@ -55,6 +81,34 @@ export default class HomeTab extends Component {
         return (
             <Container style={style.container}>
                 <Content>
+                    {/* 여기부터 스토리 헤더 시작 */}
+                    <View style={{ height: 100 }}>
+                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 7 }}>
+                            <Text style={{ fontWeight: 'bold' }}>Stories</Text>
+
+                            <View style={{ flexDirection: 'row', 'alignItems': 'center' }}>
+                                <Icon name="md-play" style={{ fontSize: 14 }}></Icon>
+                                <Text style={{ fontWeight: 'bold' }}> Watch All</Text>
+                            </View>
+                        </View>
+                        <View style={{ flex: 3 }}>
+                            <ScrollView
+                                horizontal={true}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerStyle={{
+                                    alignItems: 'center',
+                                    paddingStart: 5,
+                                    paddingEnd: 5
+                                }}>
+                                {
+                                    this.state.followings.map(following => <Thumbnail 
+                                        style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+                                        source={{uri: `https://steemitimages.com/u/${following}/avatar` }} />)
+                                }
+                            </ScrollView>
+                        </View>
+                    </View>
+                    {/* 여기까지 스토리 헤더 끝 */}
                     {
                         this.state.feeds.map(feed => <CardComponent data={ feed } key={ feed.url }/>)
                     }
